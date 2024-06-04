ea790361c4104eab93515a4bcb36bd28e6e510cb
Obada-Jawabreh <obadajawabreh6@gmail.com>
2024-06-05 01:52:16 +0300

    EX-2

--- js.js
+++ js.js
@@ -9,7 +9,13 @@ alert("Welcome MR: "+name)
 }
 else{alert("Welcome Ms: "+name)}
 
+
 let orderType = prompt("Do you want to order Donut, Coffee, Ice cream, or Bakery?");
 let orderName = prompt("Please write the name of your order:");
 alert("Your " + orderName + " is getting prepared!");
-console.log(userName + " ordered " + orderName + " (" + orderType + ").");
+console.log(name + " ordered " + orderName + " (" + orderType + ").");
+
+let user=[name,gender,orderType,orderName];
+for(let i=1; i<user.length;i++ ){
+    console.log(user[i]);
+}
