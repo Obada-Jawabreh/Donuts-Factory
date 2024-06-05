3209f3615b9994047c07f5b47dad8997ed62f090
Obada-Jawabreh <obadajawabreh6@gmail.com>
2024-06-05 23:21:11 +0300

    bransh

--- js.js
+++ js.js
@@ -1,21 +1,27 @@
-let name = prompt("Please enter your name")
-let gender =prompt("Enter your gender (male or female)")
 
-while(gender!="male"&& gender!= "female"){
-    gender =prompt("please enter your gendar")
+function askgender(){
+    let gender =prompt("Enter your gender (male or female)")
+    
+    while(gender!="male"&& gender!= "female"){
+        gender =prompt("please enter your gendar")
+    }
+    return gender;
 }
-if(gender==="male"){
-alert("Welcome MR: "+name)
-}
-else{alert("Welcome Ms: "+name)}
+let name = prompt("Please enter your name");
+let gender = askgender();
 
+if (gender === "male") {
+    alert("Welcome Mr: " + name);
+} else {
+    alert("Welcome Ms: " + name);
+}
 
 let orderType = prompt("Do you want to order Donut, Coffee, Ice cream, or Bakery?");
 let orderName = prompt("Please write the name of your order:");
 alert("Your " + orderName + " is getting prepared!");
 console.log(name + " ordered " + orderName + " (" + orderType + ").");
 
-let user=[name,gender,orderType,orderName];
-for(let i=1; i<user.length;i++ ){
+let user = [name, gender, orderType, orderName];
+for (let i = 1; i < user.length; i++) {
     console.log(user[i]);
 }
